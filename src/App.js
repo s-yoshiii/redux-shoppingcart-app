0777b2863de0c0535c85f9fe7c3941faa7c36a8a
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-09-06 23:54:58 +0900

    カート状態が変化するたびに発火する仕組みを構築しよう

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/CartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
   return (
     <main>
       <Navbar />
