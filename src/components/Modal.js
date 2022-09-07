eb0b1d79f027cf3f78d851d1bfcda18bbad84ba9
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-09-08 00:23:24 +0900

    商品全削除ボタンとキャンセルボタンを実装しよう

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/CartSlice";
+import { closeModal } from "../features/modal/ModalSlice";
 
 const Modal = () => {
+  const dispatch = useDispatch();
   return (
     <aside className="modal-container">
       <div className="modal">
         <h4>買い物かごを全て空にしますか？</h4>
         <div className="btn-container">
-          <button className="btn confirm-btn">OK</button>
-          <button className="btn clear-btn">やめておく</button>
+          <button
+            className="btn confirm-btn"
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
+            OK
+          </button>
+          <button
+            className="btn clear-btn"
+            onClick={() => dispatch(closeModal())}
+          >
+            やめておく
+          </button>
         </div>
       </div>
     </aside>
